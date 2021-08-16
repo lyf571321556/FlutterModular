a9cc2f4f36f918f1bf72e38588b6f469ba50d525
yf L <lyf571321556@gmail.com>
2021-08-16 17:17:47 +0800

    add: 配置app主题，开启IOS侧滑关闭页面

--- lib/AppWidget.dart
+++ lib/AppWidget.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       // set your initial route
+      theme: ThemeData.light().copyWith(
+          primaryColor: Colors.blueAccent,
+          accentColor: Colors.blueAccent,
+          hintColor: Colors.grey.withOpacity(0.5),
+          highlightColor: Colors.lightBlueAccent,
+          indicatorColor: Colors.white,
+          platform: TargetPlatform.iOS),
       initialRoute: "/",
       // navigatorKey: ModularApp/.navigatorKey,
       // add Modular to manage the routing system
